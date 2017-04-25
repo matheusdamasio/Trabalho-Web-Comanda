376dd1840fd599ac9b35083829a0ce684a0288ad
Juliano Afonso <julianoafonsso1@gmail.com>
2017-04-25 18:29:22 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <title>Seu produto</title>
+  <meta charset="utf-8">
+ 
+  <!-- Isso é necessário para funcionar a versão mobile -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link rel="stylesheet" type="text/css" href="//assets.locaweb.com.br/locastyle/3.10.0/stylesheets/locastyle.css">
+
+ 
+</head>
+
+<style type="text/css">
+  .ls-box-gray
+    {
+      background-color: gray;
+    }
+
+
+</style>
+<body>
+<div class="ls-box">
+  <h3> Titulo </h3>
+  <div class="ls-sm-box">
+    <form action="" class="ls-form">
+      <fieldset>
+        <label class="ls-label col-md-3">
+          <b class="ls-label-text">Nome</b>
+          <p class="ls-label-info">Digite seu nome completo</p>
+          <input type="text" name="nome" placeholder="Nome e sobrenome" required >
+        </label>
+
+    </form>
+    
+  </div>
+  
+
+</div>
+
+
+
+</body>
+ 
+ 
+  <!-- Scripts - Atente-se na ordem das chamadas -->
+  <script src="//assets.locaweb.com.br/locastyle/3.10.0/javascripts/locastyle.js"></script>
+
+</body>
+</html>
---
+++ map.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html class="ls-theme-green">
+  <head>
+    <title>Página com a estrutura inicial</title>
+
+    <meta charset="utf-8">
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="description" content="Insira aqui a descrição da página.">
+    <link href="http://assets.locaweb.com.br/locastyle/3.10.0/stylesheets/locastyle.css" rel="stylesheet" type="text/css">
+    <link rel="icon" sizes="192x192" href="/locawebstyle/assets/images/ico-boilerplate.png">
+    <link rel="apple-touch-icon" href="/locawebstyle/assets/images/ico-boilerplate.png">
+  </head>
+  <body>
+    <div class="ls-topbar">
+    <center>
+	    <h1 class="ls-brand-name">
+	      <!--<a href="home" class="ls-ico-earth"> </a> -->   
+	      	 Mapa de mesas
+	      </center>    
+	    </h1>
+	</center>
+	  <!-- Nome do produto/marca sem sidebar quando for o pre-painel  -->
+	</div>
+
+    <!-- We recommended use jQuery 1.10 or up -->
+    <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="http://assets.locaweb.com.br/locastyle/3.10.0/javascripts/locastyle.js" type="text/javascript"></script>
+  </body>
+</html>
